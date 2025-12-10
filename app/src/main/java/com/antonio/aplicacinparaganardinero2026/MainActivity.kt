83142b0b7ccf396248c98ad89e19645baaaa50d3
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-10 20:55:23 +0100

    Ultima modificación. Versión del proyecto 1.0

--- app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
@@ -3,15 +3,17 @@ package com.antonio.aplicacinparaganardinero2026
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import com.antonio.aplicacinparaganardinero2026.screen.HomeScreen
 import com.antonio.aplicacinparaganardinero2026.ui.theme.AplicaciónParaGanarDinero2026Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            AplicaciónParaGanarDinero2026Theme {}
+            AplicaciónParaGanarDinero2026Theme {
+                HomeScreen()
+            }
         }
     }
 }
