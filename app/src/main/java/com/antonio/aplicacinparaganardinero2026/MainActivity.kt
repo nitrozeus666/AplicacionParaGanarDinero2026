91fe3915223af70e19e1dc00d9e161246f893ef1
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-11 09:45:36 +0100

    Modificacion del MainActivity.kt

--- app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
@@ -3,17 +3,30 @@ package com.antonio.aplicacinparaganardinero2026
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.antonio.aplicacinparaganardinero2026.screen.HomeScreen
 import com.antonio.aplicacinparaganardinero2026.ui.theme.AplicaciónParaGanarDinero2026Theme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            AplicaciónParaGanarDinero2026Theme {
-                HomeScreen()
+    class MainActivity: AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                AplicaciónParaGanarDinero2026Theme {
+                    HomeScreen(
+                        onLanguageChange = { languageCode ->
+                            changeLanguage(languageCode)
+                        }
+                    )
+                }
             }
+        }
+
+        private fun changeLanguage(languageCode: String) {
+            val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(languageCode)
+            AppCompatDelegate.setApplicationLocales(appLocale)
         }
     }
 }
