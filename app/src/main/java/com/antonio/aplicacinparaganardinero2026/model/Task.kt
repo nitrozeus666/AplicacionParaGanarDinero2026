556e46d87ffc0548cd0efd433e349ac18212ae84
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-11 09:12:21 +0100

    Modificacion de la data class Task.kt

--- app/src/main/java/com/antonio/aplicacinparaganardinero2026/model/Task.kt
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/model/Task.kt
@@ -1,11 +1,12 @@
 package com.antonio.aplicacinparaganardinero2026.model
 
+import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class Task(
     val id: Int,
-    val title: String,
-    val description: String,
+    @StringRes val title: Int,                                //cambio: String -> Int
+    @StringRes val description: Int,                         //cambio: String -> Int
     val reward: Double,
     val icon: ImageVector
 )
