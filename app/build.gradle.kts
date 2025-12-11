9c524df980c41bfbdca4677c4bb4ab7b39dfacfd
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-11 10:21:13 +0100

    Solución al problema pantallazo en blanco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 }
--- app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.antonio.aplicacinparaganardinero2026
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -9,24 +8,22 @@ import androidx.core.os.LocaleListCompat
 import com.antonio.aplicacinparaganardinero2026.screen.HomeScreen
 import com.antonio.aplicacinparaganardinero2026.ui.theme.AplicaciónParaGanarDinero2026Theme
 
-class MainActivity : ComponentActivity() {
-    class MainActivity: AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContent {
-                AplicaciónParaGanarDinero2026Theme {
-                    HomeScreen(
-                        onLanguageChange = { languageCode ->
-                            changeLanguage(languageCode)
-                        }
-                    )
-                }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AplicaciónParaGanarDinero2026Theme {
+                HomeScreen(
+                    onLanguageChange = { languageCode ->
+                        changeLanguage(languageCode)
+                    }
+                )
             }
         }
+    }
 
-        private fun changeLanguage(languageCode: String) {
-            val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(languageCode)
-            AppCompatDelegate.setApplicationLocales(appLocale)
-        }
+    private fun changeLanguage(languageCode: String) {
+        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(languageCode)
+        AppCompatDelegate.setApplicationLocales(appLocale)
     }
 }
