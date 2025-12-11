a33294a79e9dcbad448d02ff7288d19e839c0a15
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-11 11:09:34 +0100

    en el UserPreferences.kt hemos añadido el tema oscuro

--- app/src/main/java/com/antonio/aplicacinparaganardinero2026/model/UserPreferences.kt
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/model/UserPreferences.kt
@@ -1,6 +1,7 @@
 package com.antonio.aplicacinparaganardinero2026.model
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.doublePreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
@@ -12,6 +13,7 @@ private val Context.dataStore by preferencesDataStore(name = "user_prefs")
 class UserPreferences(private val context: Context) {
     companion object {
         val BALANCE_KEY = doublePreferencesKey("user_balance")
+        val THEME_KEY = booleanPreferencesKey("is_dark_mode")
     }
 
     val balance: Flow<Double> = context.dataStore.data
@@ -19,9 +21,16 @@ class UserPreferences(private val context: Context) {
             preferences[BALANCE_KEY] ?: 0.00
         }
 
+    val isDarkMode: Flow<Boolean> = context.dataStore.data
+        .map { preferences -> preferences[THEME_KEY] ?: true }
+
     suspend fun saveBalance(amount: Double) {
         context.dataStore.edit { preferences ->
             preferences[BALANCE_KEY] = amount
         }
+    }
+
+    suspend fun saveTheme(isDark: Boolean) {
+        context.dataStore.edit { preferences -> preferences[THEME_KEY] = isDark }
     }
 }
