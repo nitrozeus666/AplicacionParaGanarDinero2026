7c29383069ffc5c5993186a4d6378e60e3835882
nitrozeus666 <rubendelensanchez233@gmail.com>
2025-12-12 13:14:52 +0100

    hemos añadido la data class Competitor.kt

---
+++ app/src/main/java/com/antonio/aplicacinparaganardinero2026/model/Competitor.kt
@@ -0,0 +1,8 @@
+package com.antonio.aplicacinparaganardinero2026.model
+
+data class Competitor(
+    val rank: Int,
+    val name: String,
+    val earnings: Double,
+    val countryFlag: String
+)
